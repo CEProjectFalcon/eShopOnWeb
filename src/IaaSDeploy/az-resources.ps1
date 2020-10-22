26fc2ba1b8b03f7161678d72d8269428e6f81524
Alexandre Teoi <ateoi@microsoft.com>
2020-10-22 10:23:30 -0300

    Parametrização dos nomes de recursos no Azure

--- src/IaaSDeploy/az-resources.ps1
+++ src/IaaSDeploy/az-resources.ps1
@@ -1,39 +1,44 @@
+$azuresubscription = "fcb2616f-096b-4e78-b6ee-72db022919b0"
+$resourcegroup = "rg-eshoponweb"
+$storageaccountname = "stdeploych001"
+$sqlservername = "sql-eshoponweb-ch-001"
+$scalesetname = "vmss-web-ch-001"
+$autoscalename = "vmssas-web-ch-001"
+
 az login
 
 # Resource Group
-az group create -l brazilsouth -n rg-eshoponweb
+az group create -l brazilsouth -n $resourcegroup
 
 # VNet
-az network vnet create -g rg-eshoponweb -l brazilsouth -n vnet-ch-001 --address-prefixes 10.1.0.0/16 --subnet-name snet-web-ch-001 --subnet-prefixes 10.1.0.0/24
-az network nsg create -g rg-eshoponweb -n nsg-weballow-ch-001
-az network nsg rule create -g rg-eshoponweb --nsg-name nsg-weballow-ch-001 -n nsgr-http-ch-001 --protocol '*' --direction inbound --source-address-prefix '*' --source-port-range '*' --destination-address-prefix '*' --destination-port-range 80 443 --access allow --priority 200
+az network vnet create -g $resourcegroup -l brazilsouth -n vnet-ch-001 --address-prefixes 10.1.0.0/16 --subnet-name snet-web-ch-001 --subnet-prefixes 10.1.0.0/24
 
-# Deployment storage
-az storage account create -g rg-eshoponweb -n stdeploych001 -l brazilsouth --sku Standard_LRS --encryption-services blob
-az ad signed-in-user show --query objectId -o tsv | az role assignment create --role "Storage Blob Data Contributor" --assignee ateoi@microsoft.com --scope "/subscriptions/fcb2616f-096b-4e78-b6ee-72db022919b0/resourceGroups/rg-eshoponweb/providers/Microsoft.Storage/storageAccounts/stdeploych001"
-az storage container create -n stcdeploych001 --account-name stdeploych001 --public-access off --auth-mode login
-az storage share create -n products --quota 10 --account-name stdeploych001
-az storage share create -n dataprotection --quota 10 --account-name stdeploych001
-az storage account keys list -g rg-eshoponweb -n stdeploych001
+# Storage
+az storage account create -g $resourcegroup -n $storageaccountname -l brazilsouth --sku Standard_LRS --encryption-services blob
+az ad signed-in-user show --query objectId -o tsv | az role assignment create --role "Storage Blob Data Contributor" --assignee ateoi@microsoft.com --scope "/subscriptions/$azuresubscription/resourceGroups/$resourcegroup/providers/Microsoft.Storage/storageAccounts/$storageaccountname"
+az storage container create -n stcdeploych001 --account-name $storageaccountname --public-access off --auth-mode login
+az storage share create -n products --quota 10 --account-name $storageaccountname
+az storage share create -n dataprotection --quota 10 --account-name $storageaccountname
+az storage account keys list -g $resourcegroup -n $storageaccountname
 
 # Azure SQL Database
-az sql server create -g rg-eshoponweb -n sql-eshoponweb-ch-001 -l brazilsouth --admin-user eshopadmin --admin-password s3nh@Comple*4
-az sql server firewall-rule create -g rg-eshoponweb -n sqlfw-allowazureips-ch-001 -s sql-eshoponweb-ch-001 --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
-az sql db create -g rg-eshoponweb -n Microsoft.eShopOnWeb.CatalogDb -s sql-eshoponweb-ch-001 -e Basic
-az sql db create -g rg-eshoponweb -n Microsoft.eShopOnWeb.Identity -s sql-eshoponweb-ch-001 -e Basic
+az sql server create -g $resourcegroup -n $sqlservername -l brazilsouth --admin-user eshopadmin --admin-password s3nh@Comple*4
+az sql server firewall-rule create -g $resourcegroup -n sqlfw-allowazureips-ch-001 -s $sqlservername --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+az sql db create -g $resourcegroup -n Microsoft.eShopOnWeb.CatalogDb -s $sqlservername -e Basic
+az sql db create -g $resourcegroup -n Microsoft.eShopOnWeb.Identity -s $sqlservername -e Basic
 
 # VM Scale Set
-az vmss create -g rg-eshoponweb -n vmss-web-ch-001 --image UbuntuLTS --upgrade-policy-mode automatic --admin-username eshopadmin --generate-ssh-keys
+az vmss create -g $resourcegroup -n $scalesetname --image UbuntuLTS --upgrade-policy-mode automatic --admin-username eshopadmin --generate-ssh-keys
 ## DNS Name
-az network public-ip list -g rg-eshoponweb --query "[?contains(name, 'vmss-web-ch-001')].name"
-az network public-ip update -g rg-eshoponweb -n vmss-web-ch-001LBPublicIP --dns-name eshoponwebch001
+$publicip = az network public-ip list -g $resourcegroup --query "[?contains(name, '$scalesetname')].name" | ConvertFrom-Json
+az network public-ip update -g $resourcegroup -n $publicip --dns-name eshoponwebch001
 ## Application deployment
-az vmss extension set -g rg-eshoponweb -n CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --vmss-name vmss-web-ch-001 --settings .\script-config.json --protected-settings .\protected-config.json
+az vmss extension set -g $resourcegroup -n CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --vmss-name $scalesetname --settings .\script-config.json --protected-settings .\protected-config.json
 ## Load Balancer configuration
-az network lb list -g rg-eshoponweb --query "[?contains(name, 'vmss-web-ch-001')].name"
-az network lb probe create -g rg-eshoponweb -n healtchecks --lb-name vmss-web-ch-001LB --protocol http --port 80 --path /health
-az network lb rule create -g rg-eshoponweb -n lbr-web-ch-001 --lb-name vmss-web-ch-001LB --backend-pool-name vmss-web-ch-001LBBEPool --backend-port 80 --frontend-ip-name loadBalancerFrontEnd --frontend-port 80 --protocol tcp --probe-name healthchecks --load-distribution SourceIP
+$loadbalancer = az network lb list -g $resourcegroup --query "[?contains(name, '$scalesetname')].name" | ConvertFrom-Json
+az network lb probe create -g $resourcegroup -n healtchecks --lb-name $loadbalancer --protocol http --port 80 --path /health
+az network lb rule create -g $resourcegroup -n lbr-web-ch-001 --lb-name $loadbalancer --backend-pool-name vmss-web-ch-001LBBEPool --backend-port 80 --frontend-ip-name loadBalancerFrontEnd --frontend-port 80 --protocol tcp --probe-name healthchecks
 ## Autoscale
-az monitor autoscale create -g rg-eshoponweb -n vmssas-web-ch-001 --resource vmss-web-ch-001 --resource-type Microsoft.Compute/virtualMachineScaleSets --min-count 2 --max-count 3 --count 2
-az monitor autoscale rule create -g rg-eshoponweb --autoscale-name vmssas-web-ch-001 --condition "Percentage CPU > 70 avg 5m" --scale out 1
-az monitor autoscale rule create -g rg-eshoponweb --autoscale-name vmssas-web-ch-001 --condition "Percentage CPU < 30 avg 5m" --scale in 1
+az monitor autoscale create -g $resourcegroup -n $autoscalename --resource $scalesetname --resource-type Microsoft.Compute/virtualMachineScaleSets --min-count 2 --max-count 3 --count 2
+az monitor autoscale rule create -g $resourcegroup --autoscale-name $autoscalename --condition "Percentage CPU > 70 avg 5m" --scale out 1
+az monitor autoscale rule create -g $resourcegroup --autoscale-name $autoscalename --condition "Percentage CPU < 30 avg 5m" --scale in 1
