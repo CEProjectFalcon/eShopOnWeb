6c4166bd3ad3955a52d6f1091f4dc190879e0cdd
Eddy Nakamura <ednakamu@microsoft.com>
2020-10-26 18:04:00 -0300

    atualizado para banco apartado

---
+++ src/Ordering.API/Data/Migrations/20201026210140_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ordering.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BuyerId = table.Column<string>(nullable: true),
+                    OrderDate = table.Column<DateTimeOffset>(nullable: false),
+                    ShipToAddress_Street = table.Column<string>(maxLength: 180, nullable: true),
+                    ShipToAddress_City = table.Column<string>(maxLength: 100, nullable: true),
+                    ShipToAddress_State = table.Column<string>(maxLength: 60, nullable: true),
+                    ShipToAddress_Country = table.Column<string>(maxLength: 90, nullable: true),
+                    ShipToAddress_ZipCode = table.Column<string>(maxLength: 18, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemOrdered_CatalogItemId = table.Column<int>(nullable: true),
+                    ItemOrdered_ProductName = table.Column<string>(maxLength: 50, nullable: true),
+                    ItemOrdered_PictureUri = table.Column<string>(nullable: true),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Units = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
