bf80e30c41d2b079ec224253d39952e067989d2c
Alexandre Teoi <ateoi@microsoft.com>
2020-10-26 10:29:10 -0300

    Parametrizar valores hard coded

--- src/IaaSDeploy/az-resources.ps1
+++ src/IaaSDeploy/az-resources.ps1
@@ -1,9 +1,12 @@
+$azureuser = az ad user list --display-name "Alexandre Teoi" --query [0].userPrincipalName
 $azuresubscription = "fcb2616f-096b-4e78-b6ee-72db022919b0"
 $resourcegroup = "rg-eshoponweb"
 $storageaccountname = "stdeploych001"
 $sqlservername = "sql-eshoponweb-ch-001"
 $scalesetname = "vmss-web-ch-001"
 $autoscalename = "vmssas-web-ch-001"
+$adminusername = "eshopadmin"
+$sqladminpassword = "s3nh@Comple*4"
 
 az login
 
@@ -12,20 +15,20 @@ az group create -l brazilsouth -n $resourcegroup
 
 # Storage
 az storage account create -g $resourcegroup -n $storageaccountname -l brazilsouth --sku Standard_LRS --encryption-services blob
-az ad signed-in-user show --query objectId -o tsv | az role assignment create --role "Storage Blob Data Contributor" --assignee ateoi@microsoft.com --scope "/subscriptions/$azuresubscription/resourceGroups/$resourcegroup/providers/Microsoft.Storage/storageAccounts/$storageaccountname"
+az ad signed-in-user show --query objectId -o tsv | az role assignment create --role "Storage Blob Data Contributor" --assignee $azureuser --scope "/subscriptions/$azuresubscription/resourceGroups/$resourcegroup/providers/Microsoft.Storage/storageAccounts/$storageaccountname"
 az storage container create -n stcdeploych001 --account-name $storageaccountname --public-access off --auth-mode login
 az storage share create -n products --quota 10 --account-name $storageaccountname
 az storage share create -n dataprotection --quota 10 --account-name $storageaccountname
 az storage account keys list -g $resourcegroup -n $storageaccountname
 
 # Azure SQL Database
-az sql server create -g $resourcegroup -n $sqlservername -l brazilsouth --admin-user eshopadmin --admin-password s3nh@Comple*4
+az sql server create -g $resourcegroup -n $sqlservername -l brazilsouth --admin-user $adminusername --admin-password $sqladminpassword
 az sql server firewall-rule create -g $resourcegroup -n sqlfw-allowazureips-ch-001 -s $sqlservername --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 az sql db create -g $resourcegroup -n Microsoft.eShopOnWeb.CatalogDb -s $sqlservername -e Basic
 az sql db create -g $resourcegroup -n Microsoft.eShopOnWeb.Identity -s $sqlservername -e Basic
 
 # VM Scale Set
-az vmss create -g $resourcegroup -n $scalesetname --image UbuntuLTS --upgrade-policy-mode automatic --admin-username eshopadmin --generate-ssh-keys
+az vmss create -g $resourcegroup -n $scalesetname --image UbuntuLTS --upgrade-policy-mode automatic --admin-username $adminusername --generate-ssh-keys
 ## DNS Name
 $publicip = az network public-ip list -g $resourcegroup --query "[?contains(name, '$scalesetname')].name" | ConvertFrom-Json
 az network public-ip update -g $resourcegroup -n $publicip --dns-name eshoponwebch001
