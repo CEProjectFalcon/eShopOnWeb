c520edd03a460c7050cd7058ee197c50f7c6912b
Beatriz Matsui <bematsui@microsoft.com>
2020-10-27 13:03:21 -0300

    Including Selenium tests

---
+++ tests/UITests/PageObjects/HomePage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+
+namespace UITests.PageObjects
+{
+    public class HomePage
+    {
+
+        private static IWebElement elemento;
+        private static string texto;
+
+        public static IWebElement meuLogin(IWebDriver driver)
+        {
+            elemento = driver.FindElement(By.XPath("/html/body/div/header/div/article/section[2]/div/section/div/a"));
+
+            return elemento;
+        }
+
+        public static string verificaTextoBotao(IWebDriver driver)
+        {
+            texto = driver.FindElement(By.XPath("//*[@id='Next']")).Text;
+
+            return texto;
+        }
+    }
+}
