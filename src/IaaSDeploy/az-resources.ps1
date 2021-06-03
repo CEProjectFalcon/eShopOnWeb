f85d1d788f396ad66da0ed9ed64458b2c81fca84
Leandro Prado <69152421+leandromsft@users.noreply.github.com>
2021-06-03 15:08:52 -0300

    PR - Capitulo testes (#19)
    * Including Selenium tests
    
    * Add Unit Tests
    
    * Ajuste testes de unidade
    
    * Adding scripts for staging provisioning
    
    * Additional script changes
    
    * final commit about unit tests
    
    * Update eshoponweb-ci.yml
    
    * Update eshoponweb-ci.yml
    
    * Update eshoponweb-cd.yml
    
    * Update eshoponweb-cd.yml
    
    Co-authored-by: Beatriz Matsui <bematsui@microsoft.com>
    Co-authored-by: Augusto Araujo <auarauj@microsoft.com>
    Co-authored-by: Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
    Co-authored-by: Fernando Henrique Inocêncio Borba Ferreira <ferhenriquef@live.com>
    Co-authored-by: Leandro Silveira Prado <leandro.prado@outlook.com.br>

--- src/IaaSDeploy/az-resources.ps1
+++ src/IaaSDeploy/az-resources.ps1
@@ -1,39 +1,43 @@
-$azureuser = az ad user list --display-name "Alexandre Teoi" --query [0].userPrincipalName
-$azuresubscription = "fcb2616f-096b-4e78-b6ee-72db022919b0"
-$resourcegroup = "rg-eshoponweb"
-$storageaccountname = "stdeploych001"
-$sqlservername = "sql-eshoponweb-ch-001"
-$scalesetname = "vmss-web-ch-001"
-$autoscalename = "vmssas-web-ch-001"
+param ([Parameter(Mandatory = $true)]$environment, [Parameter(Mandatory = $true)]$username, [Parameter(Mandatory = $true)]$azuresubscription)
+
+$azureuser = az ad user list --display-name $username --query [0].userPrincipalName
+$resourcegroup = "rg-eshoponweb-$environment"
+$storageaccountname = "stdeploy$environment"
+$storagecontainername = "stcdeploy$environment"
+$sqlservername = "sql-eshoponweb-$environment"
+$scalesetname = "vmss-web-$environment"
+$autoscalename = "vmssas-web-$environment"
+$dnsName = "eshoponweb-$environment"
 $adminusername = "eshopadmin"
 $sqladminpassword = "s3nh@Comple*4"
 
 az login
 
 # Resource Group
-az group create -l brazilsouth -n $resourcegroup
+az group create -l brazilsouth -n $resourcegroup --subscription $azuresubscription
 
 # Storage
-az storage account create -g $resourcegroup -n $storageaccountname -l brazilsouth --sku Standard_LRS --encryption-services blob
+az storage account create --subscription $azuresubscription -g $resourcegroup -n $storageaccountname -l brazilsouth --sku Standard_LRS --encryption-services blob 
 az ad signed-in-user show --query objectId -o tsv | az role assignment create --role "Storage Blob Data Contributor" --assignee $azureuser --scope "/subscriptions/$azuresubscription/resourceGroups/$resourcegroup/providers/Microsoft.Storage/storageAccounts/$storageaccountname"
-az storage container create -n stcdeploych001 --account-name $storageaccountname --public-access off --auth-mode login
+az storage container create -n $storagecontainername --account-name $storageaccountname --public-access off --auth-mode login
 az storage share create -n products --quota 10 --account-name $storageaccountname
 az storage share create -n dataprotection --quota 10 --account-name $storageaccountname
-az storage account keys list -g $resourcegroup -n $storageaccountname
 
 # Azure SQL Database
-az sql server create -g $resourcegroup -n $sqlservername -l brazilsouth --admin-user $adminusername --admin-password $sqladminpassword
-az sql server firewall-rule create -g $resourcegroup -n sqlfw-allowazureips-ch-001 -s $sqlservername --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+az sql server create --subscription $azuresubscription -g $resourcegroup -n $sqlservername -l brazilsouth --admin-user $adminusername --admin-password $sqladminpassword
+az sql server firewall-rule create -g $resourcegroup -n "sql-firewall-rule" -s $sqlservername --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
 az sql db create -g $resourcegroup -n Microsoft.eShopOnWeb.CatalogDb -s $sqlservername -e Basic
 az sql db create -g $resourcegroup -n Microsoft.eShopOnWeb.Identity -s $sqlservername -e Basic
 
 # VM Scale Set
-az vmss create -g $resourcegroup -n $scalesetname --image UbuntuLTS --upgrade-policy-mode automatic --admin-username $adminusername --generate-ssh-keys
+az vmss create --subscription $azuresubscription -g $resourcegroup -n $scalesetname --image UbuntuLTS --upgrade-policy-mode automatic --admin-username $adminusername --generate-ssh-keys
 ## DNS Name
 $publicip = az network public-ip list -g $resourcegroup --query "[?contains(name, '$scalesetname')].name" | ConvertFrom-Json
-az network public-ip update -g $resourcegroup -n $publicip --dns-name eshoponwebch001
+az network public-ip update -g $resourcegroup -n $publicip --dns-name $dnsName
+
+
 ## Application deployment
-az vmss extension set -g $resourcegroup -n CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --vmss-name $scalesetname --settings .\script-config.json --protected-settings .\protected-config.json
+az vmss extension set -g $resourcegroup -n CustomScript --publisher Microsoft.Azure.Extensions --version 2.0 --vmss-name $scalesetname --settings ".\script-config-$environment.json" --protected-settings ".\protected-config-$environment.json"
 ## Load Balancer configuration
 $loadbalancer = az network lb list -g $resourcegroup --query "[?contains(name, '$scalesetname')].name" | ConvertFrom-Json
 az network lb probe create -g $resourcegroup -n healtchecks --lb-name $loadbalancer --protocol http --port 80 --path /health
